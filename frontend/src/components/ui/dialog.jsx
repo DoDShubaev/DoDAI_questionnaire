04e6189baf284c0857b9351c6250c434207cc46b
“DoDShubaev <shubaev012@gmail.com>
2025-07-03 14:40:45 +0300

    Update RTL2

--- frontend/src/components/ui/dialog.jsx
+++ frontend/src/components/ui/dialog.jsx
@@ -30,6 +30,7 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
     <DialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
+      dir="rtl"
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         className
@@ -51,6 +52,7 @@ const DialogHeader = ({
   ...props
 }) => (
   <div
+    dir="rtl"
     className={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)}
     {...props} />
 )
@@ -61,6 +63,7 @@ const DialogFooter = ({
   ...props
 }) => (
   <div
+    dir="rtl"
     className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)}
     {...props} />
 )
@@ -69,6 +72,7 @@ DialogFooter.displayName = "DialogFooter"
 const DialogTitle = React.forwardRef(({ className, ...props }, ref) => (
   <DialogPrimitive.Title
     ref={ref}
+    dir="rtl"
     className={cn("text-lg font-semibold leading-none tracking-tight", className)}
     {...props} />
 ))
@@ -77,6 +81,7 @@ DialogTitle.displayName = DialogPrimitive.Title.displayName
 const DialogDescription = React.forwardRef(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
+    dir="rtl"
     className={cn("text-sm text-muted-foreground", className)}
     {...props} />
 ))
